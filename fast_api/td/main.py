2c984c1432f70a8c588e43be2a61668e37ca69d2
mmustar <mmustar@free.fr>
2024-05-24 17:43:31 +0200

    API sans doute fonctionnelle

--- fast_api/td/main.py
+++ fast_api/td/main.py
@@ -1,14 +1,13 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-import requests
+from fastapi import Depends, FastAPI, HTTPException, status, Query
 import pandas as pd
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from typing import Annotated, List
-import sys
+from typing import Annotated, Literal, Optional
+import random
+from random import sample
+import warnings
 
 api = FastAPI()
 security = HTTPBasic()
-
-df = pd.read_csv("questions.csv")
 
 users = {
     "alice": "wonderland",
@@ -41,8 +40,8 @@ class Question:
             "remark": self.remark,
         }
 
-sub_list = ["BDD", "Systèmes distribués","Streaming de données", "Docker", "Classification", "Systèmes distribués", "Data Science", "Machine Learning", "Automation", "Streaming de données"]
 use_list = ["Test de positionnement", "Total Bootcamp", "Test de validation"]
+sub_list = ["BDD", "Systèmes distribués", "Streaming de données", "Docker", "Classification", "Data Science", "Machine Learning", "Automation"]
 
 def login(credentials: Annotated[HTTPBasicCredentials, Depends(security)],):
     current_username = credentials.username
@@ -55,38 +54,50 @@ def login(credentials: Annotated[HTTPBasicCredentials, Depends(security)],):
         )
     return current_username
 
-def filter(subjects,use):
-    db = []
-    if use is None and subjects is not None:
-        db = df[df["subjects"].isin(subjects)]
-    elif use is not None and subjects is None:
-        db = (df["use"] == use )
-    elif use is None and subjects is None:
-        db = df
-    else:
-        db = df[(df['use'] == use) & (df['subjects'].isin(subjects))]
-    return db
-
 @api.get("/")
 def running():
     return "API OK"
 
-@api.post("/questions")
-def do_qcm(subjects: list = None, use: str = None, count: int = 5):
-    if count not in [5, 10, 20]:
-        raise ValueError("Invalid 'count' value. Must be 5, 10, or 20.")
-    if use is not None and use not in use_list:
-        raise ValueError("Invalid value for 'use'. Available values are : Test de positionnement, Total Bootcamp,  Test de validation")
-    if subjects is not None and any (subject not in sub_list for subject in subjects):
-        raise ValueError("Invalid value for 'subjects'. Available values are BDD, Systèmes distribués, Streaming de données, Docker, Classification, Systèmes distribués, Data Science, Machine Learning, Automation, Streaming de données")
-    db = filter(subjects, use)
-    qcm = db.sample(n=count, replace=False)
-    return qcm
-
 @api.get("/admin")
-def add_question(question : str, subject : str, use : str, correct : str,  responseA : str, responseB : str, responseC : str,responseD : str, remark : str, current_user: str = Depends(login)):
+def add_question(question : str, subject : str, use : str, correct : str,  responseA : str, responseB : str, responseC : str = None, responseD : str = None, remark : str =None, current_user: str = Depends(login)):
     if current_user != "admin":
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Admin privileges required")
     new_question = Question(question, subject, use, correct, responseA, responseB, responseC, responseD, remark)
     question_df = pd.DataFrame([new_question.q_data()])
     question_df.to_csv("questions.csv", mode='a', header=False, index=False)
+    return  {"message": "question added successfully"}
+
+
+@api.get("/qcm")
+def qcm_gen(
+    use: str = Query(None, description="Possible Values : 'Test de positionnement', 'Total Bootcamp', or 'Test de validation'"),
+    subjects: list[str] = Query(None, description="Possible values can be one or multiple of 'BDD', 'Systèmes distribués', 'Streaming de données', 'Docker', 'Classification', 'Data Science', 'Machine Learning', 'Automation'"),
+    count: int = Query(5, description="Available values 5, 10 or 20"),
+    current_user: str = Depends(login)
+):
+    if count not in {5, 10, 20}:
+        raise HTTPException(status_code=400, detail="Available values for count are 5, 10, or 20")
+    df = pd.read_csv("questions.csv")
+    df = df.fillna("") 
+    if use and use not in use_list:
+        raise HTTPException(status_code=400, detail="Invalid use value. : 'Test de positionnement', 'Total Bootcamp', or 'Test de validation'")
+    if subjects:
+        for subject in subjects:
+            if subject not in sub_list:
+                raise HTTPException(status_code=400, detail="Invalid subject. Possible values can be one or multiple of 'BDD', 'Systèmes distribués', 'Streaming de données', 'Docker', 'Classification', 'Data Science', 'Machine Learning', 'Automation'")
+    
+    if use:
+        df = df[df['use'] == use]
+    if subjects:
+        df = df[df['subject'].isin(subjects)]
+    
+    if len(df) < 5:
+        raise HTTPException(status_code=400, detail="Question pool is too short. Please change parameters.")
+    
+    if len(df) < count:
+        warnings.warn("Question pool is too short. All available questions are being provided.")
+        count = len(df)
+    
+    df_index = random.sample(list(df.index),count)
+    qcm = df.loc[df_index]
+    return  qcm
